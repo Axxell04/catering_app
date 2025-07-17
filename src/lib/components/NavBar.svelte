1032dd932967219a7a1dadebf86a8a44d56f4fbe
Axxell04-UTM <alajones1404@utm.edu.ec>
2025-07-17 12:59:37 -0500

    first commit

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,19 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">
+                Inicio
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                Menu
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                Pedidos
+            </a>
+        </li>
+    </ul>
+</nav>
